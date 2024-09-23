f6c16005f8a6191c27591c7043454909c4b163b1
Brian Giusti <briangiusti@Brians-MacBook-Pro-2.local>
2024-09-23 19:59:31 -0400

    More styles and stuff

--- src/LetterMagnet.jsx
+++ src/LetterMagnet.jsx
@@ -1,27 +1,26 @@
 import React from "react";
 import { useDraggable } from "@dnd-kit/core";
 
-export default function LetterMagnet({ position, id, src, style }) {
+export default function LetterMagnet({ position, id, src }) {
   const { attributes, listeners, setNodeRef, transform } = useDraggable({ id });
 
-  const mergedStyle = {
-    ...style, // Default styles passed from parent (width, height, etc.)
-    top: position.y,
-    left: position.x,
+  const magnetStyle = {
+    top: `${position.y}px`,
+    left: `${position.x}px`,
     transform: transform
       ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
       : undefined,
   };
 
   return (
-    <div 
-      ref={setNodeRef} 
-      style={mergedStyle} 
-      className="magnet" // Add class for magnet styling
-      {...listeners} 
+    <div
+      ref={setNodeRef}
+      className="magnet"
+      style={magnetStyle}
+      {...listeners}
       {...attributes}
     >
-      <img src={src} alt="My Image" style={{ width: '100%', height: '100%' }} />
+      <img src={src} alt="Letter Magnet" style={{ width: "100%", height: "100%" }} />
     </div>
   );
 }
